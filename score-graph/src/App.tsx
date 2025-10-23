c730170bad6c31ef4d027c6d0a2e1fb811421e1c
Ojas <149180655+ojasarora77@users.noreply.github.com>
2025-10-23 16:07:20 +0100

    CRM init

---
+++ score-graph/src/App.tsx
@@ -0,0 +1,41 @@
+import React, { useEffect } from 'react';
+import Header from './components/Header';
+import CryptoGrid from './components/CryptoGrid';
+import Chart from './components/Chart';
+import Sidebar from './components/Sidebar';
+import StatusBar from './components/StatusBar';
+import Particles from './components/Particles';
+
+function App() {
+  return (
+    <div className="relative min-h-screen bg-black text-white overflow-hidden">
+      <div className="absolute inset-0 z-0">
+        <Particles
+          particleColors={['#ffffff', '#ffffff']}
+          particleCount={200}
+          particleSpread={10}
+          speed={0.1}
+          particleBaseSize={100}
+          moveParticlesOnHover={true}
+          alphaParticles={false}
+          disableRotation={false}
+        />
+      </div>
+      <div className="relative z-10 container mx-auto p-4">
+        <Header />
+        <CryptoGrid />
+        <div className="grid grid-cols-1 lg:grid-cols-3 gap-8 mt-8">
+          <div className="lg:col-span-2">
+            <Chart />
+          </div>
+          <div>
+            <Sidebar />
+          </div>
+        </div>
+        <StatusBar />
+      </div>
+    </div>
+  );
+}
+
+export default App;
